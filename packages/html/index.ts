92c9552ce4d277f4b8d97acef8118e6ef5486ee2
bglee <deptno@gmail.com>
2018-05-30 22:08:35 +0900

    apply R, history log

--- packages/html/index.ts
+++ packages/html/index.ts
@@ -2,26 +2,30 @@ import {JSDOM} from 'jsdom'
 import * as R from 'ramda'
 
 export function sanitize(html: string): string[] {
-  const textNode = (node: Node): boolean => {
-    if (!node.firstChild) {
-      return false
-    }
-    return node.firstChild.nodeType === node.TEXT_NODE
-  }
-  const breathe = (x: string) => `${x}, `
-  const useless = (name: string) => ['HTML', 'HEAD', 'BODY', 'FIGURE', 'IMG', 'FIGCAPTION'].some(R.equals(name))
+  const textNode = R.ifElse(
+    R.isNil,
+    R.F,
+    R.converge(R.equals, [
+      R.path(['firstChild', 'nodeType']),
+      R.prop('TEXT_NODE')]))
+  const breathe = R.concat(R.__, ', ') as any as (_: string) => string
+  // const useless = (tag: string) => R.any(R.equals(tag), ['html', 'head', 'body', 'figure', 'img', 'figcaption'])
+  const useless = R.compose(
+    R.any(R.__, ['html', 'head', 'body', 'figure', 'img', 'figcaption']) as any as (_: any) => boolean,
+    R.equals
+  )
   const substitution = R.compose(
     breathe,
     R.cond([
       [R.equals('H3'), R.always('제목')],
-      [R.equals('H4'), R.always('부제')],
-    ]),
-  )
-  const needReplace = (node: Node) => ['H3', 'H4'].some(R.equals(node.nodeName))
+      [R.equals('H4'), R.always('부제')]]))
+  const needReplace = R.compose(
+    R.any(R.__, ['H3', 'H4']) as any as (_: any) => boolean,
+    R.equals,
+    R.prop<keyof Node, Node>('nodeName'))
   const replaceTag = R.cond([
     [needReplace, (node: Node) => substitution(node.nodeName) + node.textContent],
-    [R.T, (node: Node) => node.textContent]
-  ])
+    [R.T, (node: Node) => node.textContent]])
 
   return elements(html)
     .filter(textNode)
